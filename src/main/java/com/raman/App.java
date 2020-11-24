07748f36e6ba9b03a8d16e3ff070171e791952b3
NavyashreegowdaS <73306610+NavyashreegowdaS@users.noreply.github.com>
2020-11-24 19:38:48 +0530

    Update App.java

--- src/main/java/com/raman/App.java
+++ src/main/java/com/raman/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello from developer i am chaning the code " );
+        System.out.println( "Hello from developer i am chaning the codes " );
     }
 }
