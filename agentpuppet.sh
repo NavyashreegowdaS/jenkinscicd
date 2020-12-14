ca9379b9dbbf9d0205a1a9786b6e403a70fecef2
NavyashreegowdaS <73306610+NavyashreegowdaS@users.noreply.github.com>
2020-12-14 11:10:37 +0530

    Update agentpuppet.sh

--- agentpuppet.sh
+++ agentpuppet.sh
@@ -2,12 +2,10 @@
 wget https://apt.puppetlabs.com/puppet6-release-bionic.deb
 sudo dpkg -i puppet6-release-bionic.deb
 sudo apt update
-sudo apt install -y puppetserver
-sudo service puppetserver start
-cat >> /etc/puppetlabs/puppet/puppet.conf <<EOF
-dns_alt_name = puppet
+sudo apt install -y puppet-agent
+sudo service puppet start
+cat >> /etc/puppetlabs/puppet/puppet.conf << EOF
 [main]
-certname = puppet
 server = puppet
-environment = production
 EOF
+
